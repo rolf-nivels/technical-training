80e1d25f7a984c5e68a0a949c9b2cd8da64df553
Rolf Ackermann <rolf@weon.ch>
2023-11-06 13:49:18 +0100

    [ADD] Active field

--- estate/models/property.py
+++ estate/models/property.py
@@ -4,13 +4,14 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate properties"
 
-    name = fields.Char(string="Name of the property", required=True)
+    name = fields.Char(string="Name of the property", required=True, default="Unknown")
+    active = fields.Boolean()
     description = fields.Text()
     postcode = fields.Char(string="Post code (ZIP)")
-    date_availability = fields.Date(string="Availabilty date", copy=False)
+    date_availability = fields.Date(string="Availabilty date", default=lambda self: fields.Date.add(fields.Date.today(), months=+3), copy=False)
     expected_price = fields.Float(string="Price (expected)", required=True)
     selling_price = fields.Float(string="Price (selling)", readonly=True, copy=False)
-    bedrooms = fields.Integer(string="Size of bedrooms")
+    bedrooms = fields.Integer(string="Size of bedrooms", default=2)
     living_area  = fields.Integer(string="Size of living area")
     facades = fields.Integer()
     garage = fields.Boolean(string="Garage available?")
@@ -20,3 +21,4 @@ class EstateProperty(models.Model):
                                            ('south', 'South'),
                                            ('east', 'East'),
                                            ('west', 'West')])
+    last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
