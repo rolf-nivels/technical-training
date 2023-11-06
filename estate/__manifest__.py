520c0a94641e05451701a026bfe27b06880cc459
Rolf Ackermann <rolf@weon.ch>
2023-11-06 11:44:17 +0100

    [ADD] Actions

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,7 @@
     "depends": ["base"],  # dependencies
     "data": [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
     ],
     "installable": True,
     'license': 'LGPL-3',
